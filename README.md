3b59a5fa363f577996f170198b6eb4f4f76124c8
alfred sang <i5ting@126.com>
2014-12-03 22:26:22 +0800

    聊天界面以及通讯逻辑处理，包括左右cell的模板

--- README.md
+++ README.md
@@ -29,4 +29,36 @@ type
 - p2g 群聊
 - p2p 私聊
 
+#### 左侧cell模板
+
+ 			<li class="msgitem leftitem clearfix">
+ 				<div class="chathead pull-left">
+ 					<a target="_blank" href="#">
+ 						<img src="../images/defaultimg.jpg" alt="">
+ 					</a>
+ 				</div>
+ 				<div class="msg-content-header pull-left"></div>
+ 				<div class="msg-content-body pull-left">
+ 					<a class="close" href="#" style="display: none;"></a>
+ 					<div class="msg-content">
+ 						<p class="abstract">jdjfkdskffjdkssfdsfdsfsefsf</p>
+ 						<span class="chat-time text-right">2012-5-28 3:35</span>
+ 					</div>
+ 				</div>
+ 			</li>
+			
+#### 右侧cell模板			
+
+			<li class="msgitem rightitem clearfix">
+ 				<div class="msg-content-header pull-right"></div>
+ 				<div class="msg-content-body pull-right">
+ 					<a class="close" href="#" style="display: none;"></a>
+ 					<div class="msg-content">
+ 						<p class="abstract">jdjfkdskffjdkjdjfkdskffjdkssfdsfdsfsefsfjdjfkdkffjdkssfdsfdsfsefsfjdjfkdskffjdkssfdsfkffjdkssfdsfdsfsefsfjdjfkdskffjdkssfdsfkffjdkssfdsfdsfsefsfjdjfkdskffjdkssfdsfskffjdkssfdsfkffjdkssfdsfdsfsefsfjdjfkdskffjdkssfdsfkffjdkssfdsfdsfsefsfjdjfkdskffjdkssfdsfdsfsefsfssfdsfdsfsefsf</p>
+ 						<span class="chat-time text-right">2012-5-28 3:35</span>
+ 					</div>
+ 				</div>
+ 				<div class="clear"></div>
+ 			</li>
+
 
--- src/js/chat.js
+++ src/js/chat.js
@@ -1,6 +1,9 @@
 Zepto(function($){
   console.log('Ready to Zepto!')
-	
+		
+	function log(t){
+		console.log('[LOG] '+ t);
+	}
 	var client = new iChatClient({
 		url : 'http://at35.com:4567/faye',
 		timeout : 120,
@@ -9,39 +12,93 @@ Zepto(function($){
 
 	
 	function bind_send_msg_event(){
+		var input_text = $.trim($('#msg_input_text_id').val());
+		log('发送信息内容是：'+input_text);
+		
 		$('.send_msg_btn').click(function(){
-			alert('send_msg_btn');
+			// alert('send_msg_btn');
 			client.send('foo',{
-				text:'dssdjfkjkl'
+				text: input_text
 			},function(){
-				alert('Message received by server!');
+				// alert('Message received by server!');
 			},function(error){
 				alert('There was a problem: ' + error.message);
 			});
 		});
 	}
-	
-	setTimeout(function(){
-		client.send('foo',{
-			text:'dssdjfkjkl'
-		},function(){
-			alert('Message received by server!');
-		},function(error){
-			alert('There was a problem: ' + error.message);
-		});
-	},1000);
+
+	function test(){
+		setTimeout(function(){
+			client.send('foo',{
+				text:'dssdjfkjkl'
+			},function(){
+				// alert('Message received by server!');
+			},function(error){
+				alert('There was a problem: ' + error.message);
+			});
+		},1000);
+	}
 	
 	function leave(){
 		client.leave('foo');
 	}
+	
+	function write_msg_content_to_dom(msg){
+		var is_myself = false;
+		
+		if(is_myself == true){
+			write_left_msg_content_to_dom(msg);
+		}else{
+			write_right_msg_content_to_dom(msg);
+		}
+	}
+	
+	function write_left_msg_content_to_dom(msg){
+		var received_msg_html = "<li class='msgitem leftitem clearfix'>"
+			+"	<div class='chathead pull-left'>"
+			+"		<a target='_blank' href='#'>"
+			+"			<img src='../images/defaultimg.jpg' alt=''>"
+			+"		</a>"
+			+"	</div>"
+			+"	<div class='msg-content-header pull-left'></div>"
+			+"	<div class='msg-content-body pull-left'>"
+			+"		<a class='close' href='#' style='display: none;'></a>"
+			+"		<div class='msg-content'>"
+			+"			<p class='abstract'>" + msg.text + "</p>"
+			+"			<span class='chat-time text-right'>2012-5-28 3:35</span>"
+			+"		</div>"
+			+"	</div>"
+			+"</li>";
+		
+		$('#chat_container_id').append(received_msg_html);
+	}
+	
+	function write_right_msg_content_to_dom(msg){
+		var received_msg_html = "<li class='msgitem rightitem clearfix'>"
+ 				+"<div class='msg-content-header pull-right'></div>"
+ 				+"<div class='msg-content-body pull-right'>"
+ 					+"<a class='close' href='#' style='display: none;'>"+"</a>"
+ 					+"<div class='msg-content'>"
+ 						+"<p class='abstract'>"+ msg.text +"</p>"
+ 						+"<span class='chat-time text-right'>2012-5-28 3:35</span>"
+ 					+"</div>"
+ 				+"</div>"
+ 				+"<div class='clear'>"+"</div>"
+ 			+"</li>"
+		
+		$('#chat_container_id').append(received_msg_html);
+	}
+	
+		
 	
 	init();
 	
 	function init(){
 		client.join('foo', function(message) {
 		  // handle message
-			alert(message.text);
-			console.log(message);
+			// alert(message.text);
+			write_msg_content_to_dom(message);
+			log('收到的信息是：'+message.text);
 		});
 		
 		// 绑定发送按钮事件

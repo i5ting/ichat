8a5a7237865c0a831c3b5744e3590296049dd0df
alfred sang <i5ting@126.com>
2014-12-03 17:08:30 +0800

    修改聊天左侧气泡的背景问题

--- src/js/chat.js
+++ src/js/chat.js
@@ -1,28 +1,33 @@
-var client = new iChatClient({
-	url : 'http://at35.com:4567/faye',
-	timeout : 120,
-	retry		: 5
-});
-
-client.join('foo', function(message) {
-  // handle message
-	alert(message.text);
-	console.log(message);
-});
-
-setTimeout(function(){
-	client.send('foo',{
-		text:'dssdjfkjkl'
-	},function(){
-		 // alert('Message received by server!');
-	},function(error){
-		alert('There was a problem: ' + error.message);
-	});
-
-	// client.leave('foo');
-},2000);
-
-
 Zepto(function($){
   console.log('Ready to Zepto!')
-})
+	
+	var client = new iChatClient({
+		url : 'http://at35.com:4567/faye',
+		timeout : 120,
+		retry		: 5
+	});
+
+	function init(){
+		client.join('foo', function(message) {
+		  // handle message
+			alert(message.text);
+			console.log(message);
+		});
+	}
+	
+	$('发送按钮').click(function(){
+			client.send('foo',{
+				text:'dssdjfkjkl'
+			},function(){
+				alert('Message received by server!');
+			},function(error){
+				alert('There was a problem: ' + error.message);
+			});
+	});
+	
+	function leave = function(){
+		client.leave('foo');
+	}
+	
+	init();
+});

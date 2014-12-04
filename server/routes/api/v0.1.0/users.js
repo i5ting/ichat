7d44018ea3f261f3a4f1f47ee7c32ed39b9527ad
alfred sang <i5ting@126.com>
2014-12-04 15:00:03 +0800

    返回数据在出错的时候没有值，已修正

--- server/routes/api/v0.1.0/users.js
+++ server/routes/api/v0.1.0/users.js
@@ -62,7 +62,7 @@ router.post('/login', function(req, res) {
 	  if (err) {
 	  	 console.error(err);
 			 return res.status(200).json({
-			 	 data: sur,
+			 	 data: {},
 				 status:{
 				 	 code : err.code,
 					 msg  : err.name + ' : ' + err.err

fbb868d0de9653a9c527c7435c72e1574f2cb92c
alfred sang <i5ting@126.com>
2014-12-04 14:42:01 +0800

    cors 问题，已修改，使用https://github.com/TroyGoode/node-cors

--- src/js/config.js
+++ src/js/config.js
@@ -1,22 +1,30 @@
-window.ichat_config ={
+window.ichat_config = {
 	version: 'v0.1.0',
 	chat_server_url:'127.0.0.1:4567',
+	// chat_server_url:'127.0.0.1:4567',
 	get_chat_server_url : function(){
-		retrn 'http://' + this.chat_server_url + '/faye'
+		return 'http://' + this.chat_server_url + '/faye'
 	},
-	api_server_url:'127.0.0.1:5555',
+	api_server_url:'at35.com:5555',
+	//api_server_url:'127.0.0.1:5555',
 	get_api_server_url : function(){
-		retrn 'http://' + this.api_server_url + '/api/' + this.version;
+		return 'http://' + this.api_server_url + '/api/' + this.version;
 	},
 	get_api_base_url : function(patten){
-		retrn this.get_api_server_url()+ patten
+		return this.get_api_server_url()+ patten
 	},
 	/**
 	 * 获取用户注册url
 	 */
 	get_api_user_register_url : function(){
-		retrn this.get_api_base_url('/users/new')
+		return this.get_api_base_url('/users/new')
 	},
+	/**
+	 * 用户登陆
+	 */
+	get_api_user_login_url : function(){
+		return this.get_api_base_url('/users/login')
+	}
 	
 }
 

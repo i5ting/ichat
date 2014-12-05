874d14fb00a3954425430fd1f0f8ef589cefcc84
alfred sang <i5ting@126.com>
2014-12-05 18:27:13 +0800

    终于可以把本地message存到库里了，ohyea

--- src/js/config.js
+++ src/js/config.js
@@ -1,6 +1,99 @@
 // <script src="./lib/js/iChatClient.js"></script>
 // <script src="./js/config.js"></script>
 
+/*!
+Math.uuid.js (v1.4)
+http://www.broofa.com
+mailto:robert@broofa.com
+
+Copyright (c) 2010 Robert Kieffer
+Dual licensed under the MIT and GPL licenses.
+*/
+
+/*
+ * Generate a random uuid.
+ *
+ * USAGE: Math.uuid(length, radix)
+ *   length - the desired number of characters
+ *   radix  - the number of allowable values for each character.
+ *
+ * EXAMPLES:
+ *   // No arguments  - returns RFC4122, version 4 ID
+ *   >>> Math.uuid()
+ *   "92329D39-6F5C-4520-ABFC-AAB64544E172"
+ *
+ *   // One argument - returns ID of the specified length
+ *   >>> Math.uuid(15)     // 15 character ID (default base=62)
+ *   "VcydxgltxrVZSTV"
+ *
+ *   // Two arguments - returns ID of the specified length, and radix. (Radix must be <= 62)
+ *   >>> Math.uuid(8, 2)  // 8 character ID (base=2)
+ *   "01001010"
+ *   >>> Math.uuid(8, 10) // 8 character ID (base=10)
+ *   "47473046"
+ *   >>> Math.uuid(8, 16) // 8 character ID (base=16)
+ *   "098F4D35"
+ */
+(function() {
+  // Private array of chars to use
+  var CHARS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('');
+
+  Math.uuid = function (len, radix) {
+    var chars = CHARS, uuid = [], i;
+    radix = radix || chars.length;
+
+    if (len) {
+      // Compact form
+      for (i = 0; i < len; i++) uuid[i] = chars[0 | Math.random()*radix];
+    } else {
+      // rfc4122, version 4 form
+      var r;
+
+      // rfc4122 requires these characters
+      uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-';
+      uuid[14] = '4';
+
+      // Fill in random data.  At i==19 set the high bits of clock sequence as
+      // per rfc4122, sec. 4.1.5
+      for (i = 0; i < 36; i++) {
+        if (!uuid[i]) {
+          r = 0 | Math.random()*16;
+          uuid[i] = chars[(i == 19) ? (r & 0x3) | 0x8 : r];
+        }
+      }
+    }
+
+    return uuid.join('');
+  };
+
+  // A more performant, but slightly bulkier, RFC4122v4 solution.  We boost performance
+  // by minimizing calls to random()
+  Math.uuidFast = function() {
+    var chars = CHARS, uuid = new Array(36), rnd=0, r;
+    for (var i = 0; i < 36; i++) {
+      if (i==8 || i==13 ||  i==18 || i==23) {
+        uuid[i] = '-';
+      } else if (i==14) {
+        uuid[i] = '4';
+      } else {
+        if (rnd <= 0x02) rnd = 0x2000000 + (Math.random()*0x1000000)|0;
+        r = rnd & 0xf;
+        rnd = rnd >> 4;
+        uuid[i] = chars[(i == 19) ? (r & 0x3) | 0x8 : r];
+      }
+    }
+    return uuid.join('');
+  };
+
+  // A more compact, but less performant, RFC4122v4 solution:
+  Math.uuidCompact = function() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+      return v.toString(16);
+    });
+  };
+})();
+
 // 对Date的扩展，将 Date 转化为指定格式的String
 // 月(M)、日(d)、小时(h)、分(m)、秒(s)、季度(q) 可以用 1-2 个占位符， 
 // 年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字) 
@@ -49,6 +142,12 @@ window.ichat_config = {
 		
 		db.transaction(function (tx) {	
 			tx.executeSql(sql);
+		},function(){
+			alert('fail');
+			return 0;
+		},function(){
+			// alert('succ');
+			return 1;
 		});
 	},
 	/** Select Row from Table **/ 
@@ -136,6 +235,7 @@ window.ichat_config = {
 		
 		var _msg = msg;
 		
+		var message_id = Math.uuid();
 		$.extend(_msg, {
 			// 整合用户信息
 			uid		: current_user_uid,
@@ -145,6 +245,7 @@ window.ichat_config = {
 			sid 	: current_session_id,
 			sname	: current_session_name
 		},{
+			mid: message_id,
 			timestamp : new Date().Format("yyyy-MM-dd hh:mm:ss")
 		});
 		
@@ -184,7 +285,8 @@ Class('MessageBase',{
 	}
 });
 Class('Message', MessageBase, {
-	constructor:function(uid, avatar, sid, sname, timestamp, msg){
+	constructor:function(mid, uid, avatar, sid, sname, timestamp, msg){
+		this.mid = mid;
 		this.uid = uid;
 		this.avatar = avatar;
 		this.sid = sid;
@@ -202,6 +304,7 @@ Class('Message', MessageBase, {
 	create:function(){
 		var sql = 'CREATE TABLE IF NOT EXISTS message ('
 			+'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+			+'mid string, '
 			+'uid string, '
 			+'avatar string,'
 			+'sid string,'
@@ -212,7 +315,8 @@ Class('Message', MessageBase, {
 		this.exec_sql(sql);
 	},
 	save:function(){
-		var sql = "insert into message ('uid',' avatar',' sid',' sname',' timestamp',' msg') values('"
+		var sql = "insert into message ('mid','uid','avatar','sid','sname','timestamp','msg') values('"
+				+ this.mid +"','" 
 				+ this.uid +"','" 
 				+ this.avatar + "','" 
 				+ this.sid + "',' " 
@@ -221,7 +325,6 @@ Class('Message', MessageBase, {
 				+ this.msg 
 			+ "')";
 		
-		ichat_config.log_sql(sql);
 		ichat_config.exec_sql(sql);
 	},
 	to_string:function(){
@@ -262,6 +365,7 @@ Class('SessionLisner',MessageBase, {
 		
 		this.config = ichat_config;
 		this.client = this.config.get_client();
+		this.last_msg_id = undefined;
 	},
 	start_observe:function(){
 		var current_session_id = this.session['sid'];
@@ -269,22 +373,29 @@ Class('SessionLisner',MessageBase, {
 	
 		this.client.join(current_topic, function(message) {
 		  // handle message
-			console.log('收到的信息是：'+message.text);
-		
-			//TODO: 写到websql里
-			this.save_message_to_web_sql(message);
+			
+			if(this.last_msg_id != message.mid){
+				console.log('收到的信息是：'+message.text);
+				//TODO: 写到websql里
+				this.save_message_to_web_sql(message);
+				
+				this.last_msg_id = message.mid;
+			}else{
+				console.log('收到的信息是重复的，丢弃');
+			}
 		});
 	},
 	save_message_to_web_sql:function(message){
 		var msg = new Message();
+		
+		msg.mid = message.mid;
 		msg.uid = message.uid;
 		msg.avatar = message.avatar;
 		msg.sid = message.sid;
 		msg.sname= message.sname;
 		msg.timestamp = message.timestamp;
 		msg.msg = message.text;
-		
-		this.config().log(msg.to_string());
+		 
 		
 		msg.save();
 	}

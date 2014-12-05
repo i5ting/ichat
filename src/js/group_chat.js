c248229fa26bf9656fe29ac98171cf511381ff5a
guili <cutelili8023@126.com>
2014-12-05 16:56:38 +0800

    发布群聊页面

--- src/js/group_chat.js
+++ src/js/group_chat.js
@@ -1,11 +1,16 @@
 $(function(){
-  $('.table_radio_people li input[type = checkbox]').click(function(){ 
-    var img_src = $(this).parent('li').find('span.media-object').find('img').attr('src');
-    var index = $(this).parent('li').index(); 
-    if($(this).attr("checked")){
-      $('<img src="'+img_src+'" class="img_'+index+'" />').appendTo($('div.checked_box'));
-    }else{
-      $('div.checked_box').find($('img.img_'+index+' ')).remove();
-    }
-  })
+	$('.table_radio_people li').click(function(){
+        var img_src = $(this).find('span.media-object').find('img').attr('src');
+        var index=  $(this).index(); 
+          if($(this).hasClass('false')){
+            $(this).find('input[type = checkbox]').attr('checked',true); 
+            $('<img src="'+img_src+'" class="img_'+index+'" />').appendTo($('div.checked_box'));
+            $(this).removeClass('false').addClass('true');
+          } else if($(this).hasClass('true')){
+            $(this).find('input[type = checkbox]').removeAttr('checked'); 
+            $('div.checked_box').find($('img.img_'+index+' ')).remove();
+            $(this).removeClass('true').addClass('false');
+          }  
+    })
 });
+

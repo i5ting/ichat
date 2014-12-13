e09fe49131b6151811449666c936f87c6e138de6
alfred sang <i5ting@126.com>
2014-12-14 00:00:57 +0800

    增加修改头像，可预览以及在me页面里头像展示功能的功能

--- src/js/avatar.js
+++ src/js/avatar.js
@@ -2,12 +2,28 @@ Zepto(function($){
 
 	var config = window.ichat_config;
 	
+
+	function main(){
+		var current_user = CURRENT_USER.get_current_user();	
+		var avatar = current_user.avatar;
+		var avatar_url = './images/avatar/'+ avatar +'';
+		$("#user_avatar").attr('src', avatar_url);
+	}
+	
+	main()
+	
+	
 	$('#change_avatar_btn').click(function(){
 		var a = $("select[name='avatar']").val();
 		config.change_avatar(a,function(){
 			alert('保存成功');
-		});
-		
+		});		
+	});
+	
+	$('#avatar_select').change(function(){		
+		var avatar = $("#avatar_select").val();
+		var avatar_url = './images/avatar/'+ avatar +'';
+		$("#user_avatar").attr('src', avatar_url);
 	});
 });
 
